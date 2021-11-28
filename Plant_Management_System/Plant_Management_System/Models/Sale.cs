4d9c7e252fe29540001709a774bc7faf9ae923bd
qweenkrisp <56236852+qweenkrisp@users.noreply.github.com>
2021-11-27 21:52:16 -0800

    add wishlist, trades, sale

---
+++ Plant_Management_System/Plant_Management_System/Models/Sale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plant_Management_System.Models
+{
+    public class Sale
+    {
+        public enum ListType
+        {
+            Facebook,
+            Auction,
+            PopUp
+        }
+
+        public int SaleId { get; set; }
+
+        public int PlantId { get; set; }
+
+        public double ListPrice { get; set; }
+
+        public ListType Listing { get; set; }
+
+        public DateTime DateSold { get; set; }
+
+        public int PersonId { get; set; }
+
+        public string Buyer { get; set; }
+    }
+}
---
+++ Plant_Management_System/Plant_Management_System/Models/WishList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plant_Management_System.Models
+{
+    public class WishList
+    {
+        public int WishListId { get; set; }
+
+        public string PlantName { get; set; }
+
+        public double Budget { get; set; }
+
+    }
+}
