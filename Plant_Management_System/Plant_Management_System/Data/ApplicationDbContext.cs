c63e2520190373e6c9b96458ab8a56944f4046c1
bungur <bungur@fredhutch.org>
2021-11-28 22:05:55 -0500

    Adding User MVC

--- Plant_Management_System/Plant_Management_System/Data/ApplicationDbContext.cs
+++ Plant_Management_System/Plant_Management_System/Data/ApplicationDbContext.cs
@@ -17,5 +17,6 @@ namespace Plant_Management_System.Data
         public DbSet<Plant_Management_System.Models.WishList> WishList { get; set; }
         public DbSet<Plant_Management_System.Models.Trade> Trade { get; set; }
         public DbSet<Plant_Management_System.Models.Sale> Sale { get; set; }
+        public DbSet<Plant_Management_System.Models.User> User { get; set; }
     }
 }
